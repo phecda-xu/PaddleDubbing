d05bba3d396ce943ce4f668f3f4c2a152347f949
Phecda xu <46859427+phecda-xu@users.noreply.github.com>
2022-02-23 22:06:00 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,17 +1,5 @@
 #! /bin/bash
 
-# c++编译环境
-apt-get update
-sudo apt install build-essential
-
-# python-dev 出现python.h找不到的错误时安装这个，版本与系统python的版本对应
-# sudo apt-get install python3.8-dev
-
-
-# python 环境
-python -m pip install --upgrade pip
-
-
 pip install pytest-runner -i https://pypi.tuna.tsinghua.edu.cn/simple
 pip install paddlepaddle -i https://mirror.baidu.com/pypi/simple
 pip install paddlespeech -i https://pypi.tuna.tsinghua.edu.cn/simple
