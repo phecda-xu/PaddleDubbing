986d23d6604b36fb7531705003432218072f788c
Phecda xu <46859427+phecda-xu@users.noreply.github.com>
2022-03-03 23:59:26 +0800

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,15 @@
 #! /bin/bash
 
+#! /bin/bash
+
+device=$1
+
 pip install pytest-runner -i https://pypi.tuna.tsinghua.edu.cn/simple
-pip install paddlepaddle -i https://mirror.baidu.com/pypi/simple
+
+if [ $device = 'gpu' ];then
+  pip install paddlepaddle-gpu -i https://mirror.baidu.com/pypi/simple
+else
+  pip install paddlepaddle -i https://mirror.baidu.com/pypi/simple
+fi
+
 pip install -r requirements.txt -i https://pypi.tuna.tsinghua.edu.cn/simple
